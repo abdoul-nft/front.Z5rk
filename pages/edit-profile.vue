a027214a02e1c34c0b265f88cc95e53e1951dd0b
abdoul-nft <abdoul.nft@gmail.com>
2022-05-10 19:00:26 +0200

    deploy

--- pages/edit-profile.vue
+++ pages/edit-profile.vue
@@ -1,121 +1,120 @@
 <template>
 <div id="wrapper">
-<div id="content">
-    <AppDefaultHeader title="Edit Profile" />
-    <div class="space-sticky"></div>
+    <form id="content" @submit="handleSubmit">
+        <AppDefaultHeader title="Edit Profile" />
+        <div class="space-sticky"></div>
 
-    <section class="un-creator-ptofile">
-        <!-- head -->
-        <div class="head">
-            <div class="cover-image d-flex align-items-center justify-content-center overlay">
-                <picture>
-                    <source srcset="~/assets/images/other/20.webp" type="image/webp">
-                    <img src="~/assets/images/other/20.jpg" alt="cover image">
-                </picture>
-                <div class="position-absolute">
-                    <button type="button" class="btn btn-upload-text">
-                        <input type="file">
-                        <span>Upload cover photo</span>
-                    </button>
+        <section class="un-creator-ptofile">
+            <div class="head">
+                <div class="cover-image d-flex align-items-center justify-content-center overlay">
+                    <picture>
+                        <source srcset="~/assets/images/other/20.webp" type="image/webp">
+                        <img src="~/assets/images/other/20.jpg" alt="cover image">
+                    </picture>
+                    <div class="position-absolute">
+                        <button type="button" class="btn btn-upload-text">
+                            <input type="file">
+                            <span>Upload cover photo</span>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div class="text-user-creator">
-                <div class="d-flex align-items-center">
-                    <div class="user-img d-flex align-items-center justify-content-center position-relative">
-                        <picture>
-                            <source srcset="~/assets/images/avatar/avatar.webp" type="image/webp">
-                            <img src="~/assets/images/avatar/avatar.png" alt="creator image">
-                        </picture>
-                        <div class="position-absolute">
-                            <button type="button" class="btn btn-upload-icon">
-                                <input type="file">
-                                <div class="icon">
-                                    <i class="ri-camera-line"></i>
-                                </div>
-                            </button>
+                <div class="text-user-creator">
+                    <div class="d-flex align-items-center">
+                        <div class="user-img d-flex align-items-center justify-content-center position-relative">
+                            <picture>
+                                <source srcset="~/assets/images/avatar/avatar.webp" type="image/webp">
+                                <img src="~/assets/images/avatar/avatar.png" alt="creator image">
+                            </picture>
+                            <div class="position-absolute">
+                                <button type="button" class="btn btn-upload-icon">
+                                    <input type="file">
+                                    <div class="icon">
+                                        <i class="ri-camera-line"></i>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="text">
+                            <h3 class="size-15 weight-500">Upload Profile Photo</h3>
+                            <p class="size-11 weight-400">We recommend a 300x300px JPG, PNG, SVG, WEBP or GIF (1MB
+                                max size)
+                            </p>
                         </div>
                     </div>
-                    <div class="text">
-                        <h3 class="size-15 weight-500">Upload Profile Photo</h3>
-                        <p class="size-11 weight-400">We recommend a 300x300px JPG, PNG, SVG, WEBP or GIF (1MB
-                            max size)
-                        </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="padding-20 form-edit-profile margin-b-20">
+            <div class="form-group">
+                <label>Username</label>
+                <input v-model="user.username" type="text" class="form-control"  placeholder="Enter username">
+                <div class="size-11 color-text form-text">Username is not visible to other users</div>
+            </div>
+            <div class="form-group">
+                <label>Bio</label>
+                <textarea v-model="user.bio" class="form-control" rows="3" placeholder="Tell the world your story!">
+                </textarea>
+            </div>
+            <div class="form-group">
+                <label>E-Mail Address</label>
+                <!--is-invalid color-red -->
+                <input v-model="user.email" type="email" class="form-control" 
+                    placeholder="Enter your email">
+                <div class="size-11 form-text">This email address is not valid</div>
+            </div>
+
+            <div class="bk-social-networks">
+                <h3>Social networks</h3>
+                <div class="form-group with_icon">
+                    <label>Twitter</label>
+                    <div class="input_group">
+                        <input v-model="user.twitter_username" type="text" class="form-control" placeholder="@twitter username">
+                        <div class="icon">
+                            <i class="ri-twitter-fill"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group with_icon">
+                    <label>Instagram</label>
+                    <div class="input_group">
+                        <input v-model="user.instagram_username" type="text" class="form-control" placeholder="@instagram username">
+                        <div class="icon">
+                            <i class="ri-instagram-fill"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
 
-    <section class="padding-20 form-edit-profile margin-b-20">
-        <div class="form-group">
-            <label>Username</label>
-            <input type="text" class="form-control" :value="$store.state.user.username"  placeholder="Enter username">
-            <div class="size-11 color-text form-text">Username is not visible to other users</div>
-        </div>
-        <div class="form-group">
-            <label>Bio</label>
-            <textarea  class="form-control" rows="3" placeholder="Tell the world your story!">
-            </textarea>
-        </div>
-        <div class="form-group">
-            <label>E-Mail Address</label>
-            <!--is-invalid color-red -->
-            <input type="email" class="form-control" :value="$store.state.user.email" 
-                placeholder="Enter your email">
-            <div class="size-11 form-text">This email address is not valid</div>
-        </div>
+            <div class="size-13 color-text margin-t-10 border-b border-b-solid border-snow padding-b-20">
+                To update your settings you should sign a message through your wallet. Click <span
+                    class="text-dark">'Update
+                    Profile'</span> then
+                sign the message.
+            </div>
 
-        <div class="bk-social-networks">
-            <h3>Social networks</h3>
-            <div class="form-group with_icon">
-                <label>Twitter</label>
-                <div class="input_group">
-                    <input type="text" class="form-control" :value="$store.state.user.twitter_username" placeholder="@twitter username">
-                    <div class="icon">
-                        <i class="ri-twitter-fill"></i>
+        </section>
+
+        <div class="space-sticky-footer"></div>
+
+        <footer class="footer-pages-forms">
+            <div class="content">
+                <div class="links-clear-data">
+                    <a href="page-collectibles-details.html" class="link-clear">
+                        <i class="ri-close-circle-line"></i>
+                        <span>Clear all</span>
+                    </a>
+                </div>
+                <button type="submit" aria-label="profile" class="btn btn-bid-items">
+                    <p>Update Profile</p>
+                    <div class="ico">
+                        <i class="ri-arrow-drop-right-line"></i>
                     </div>
-                </div>
+                </button>
             </div>
-
-            <div class="form-group with_icon">
-                <label>Instagram</label>
-                <div class="input_group">
-                    <input type="text" class="form-control" :value="$store.state.user.instagram_username" placeholder="@instagram username">
-                    <div class="icon">
-                        <i class="ri-instagram-fill"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="size-13 color-text margin-t-10 border-b border-b-solid border-snow padding-b-20">
-            To update your settings you should sign a message through your wallet. Click <span
-                class="text-dark">'Update
-                Profile'</span> then
-            sign the message.
-        </div>
-
-    </section>
-
-    <div class="space-sticky-footer"></div>
-
-    <footer class="footer-pages-forms">
-        <div class="content">
-            <div class="links-clear-data">
-                <a href="page-collectibles-details.html" class="link-clear">
-                    <i class="ri-close-circle-line"></i>
-                    <span>Clear all</span>
-                </a>
-            </div>
-            <a href="page-my-profile.html" aria-label="profile" class="btn btn-bid-items">
-                <p>Update Profile</p>
-                <div class="ico">
-                    <i class="ri-arrow-drop-right-line"></i>
-                </div>
-            </a>
-        </div>
-    </footer>
-</div>
+        </footer>
+    </form>
 </div>
 </template>
 
@@ -125,6 +124,27 @@ import Vue from 'vue'
 export default Vue.extend({
   middleware: 'auth',
   name: 'edit-profile',
-  layout: 'default'
+  layout: 'simple',
+  data() {
+    return {
+        user: {
+            username: '',
+            bio: '',
+            email: '',
+            twitter_username: '',
+            instagram_username: '',
+        },    
+    }
+  },
+  mounted() {
+    const { username, bio, email, twitter_username, instagram_username } = this.$store.state.user
+    this.user = { username, bio, email, twitter_username, instagram_username }
+  },
+  methods: {
+    handleSubmit(e: any){
+        e.preventDefault();
+        this.$store.dispatch('editProfile', { w_adress: this.$store.state.user.wallet_address,  data: this.user })
+    }
+  },
 })
 </script>
--- store/index.ts
+++ store/index.ts
@@ -16,28 +16,29 @@ export const getters: GetterTree<RootState, RootState> = {
 }
 
 export const mutations: MutationTree<RootState> = {
-  CHANGE_USER: (state, newVal: any) => (state.user = newVal),
-  CHANGE_BALANCE: (state, newVal: any) => (state.balance = newVal),
-  CHANGE_TRS: (state, newVal: any) => (state.transactions = newVal),
-  CHANGE_MODE: (state) => (state.mode = state.mode ? 'light' : 'dark'),
+  SET_USER: (state, newVal: any) => (state.user = newVal),
+  SET_BALANCE: (state, newVal: any) => (state.balance = newVal),
+  SET_TRS: (state, newVal: any) => (state.transactions = newVal),
+  SET_MODE: (state) => (state.mode = state.mode ? 'light' : 'dark'),
   SET_NFTS: (state, newVal: any) => (state.nfts = newVal),
+  SHOW_SOMETHING: () => (console.log('ok')),
 }
 
 export const actions: ActionTree<RootState, RootState> = {
   updateUser({ commit }, newVal) {
-    commit('CHANGE_USER', newVal)
+    commit('SET_USER', newVal)
   },
   updateBalance({ commit }, newVal) {
-    commit('CHANGE_BALANCE', newVal)
+    commit('SET_BALANCE', newVal)
   },
   updateTransactions({ commit }, newVal) {
-    commit('CHANGE_TRS', newVal)
+    commit('SET_TRS', newVal)
   },
   changeMode({ commit }) {
-    commit('CHANGE_MODE')
+    commit('SET_MODE')
   },
   getNFTs({ commit }) {
-    this.$axios.get('https://sea-turtle-app-n8fhg.ondigitalocean.app/v1/nfts/?q=bored ape')
+    this.$axios.get('https://sea-turtle-app-n8fhg.ondigitalocean.app/v1/nfts/?q=doodles')
       .then( (response)  => {
         if(response) {
           commit('SET_NFTS', response.data.data.result)
@@ -46,6 +47,29 @@ export const actions: ActionTree<RootState, RootState> = {
       .catch( (error)  => {
           console.log(error);
       });
+  },
+  editProfile({ commit }, {data, w_adress}) {
+    this.$axios.put('https://sea-turtle-app-n8fhg.ondigitalocean.app/v1/auth/account/' + w_adress, data)
+      .then( (response)  => {
+        if(response) {
+          commit('SHOW_SOMETHING')
+          this.dispatch('getCurrentUser', w_adress)
+        }
+      })
+      .catch( (error)  => {
+          console.log(error);
+      });
+  },
+  getCurrentUser({ commit }, w_adress) {
+    this.$axios.get('https://sea-turtle-app-n8fhg.ondigitalocean.app/v1/auth/account/' + w_adress)
+    .then( (data)  => {
+      if(data.data.user) {
+        commit('SET_USER', data.data.user)
+      }
+    })
+    .catch( (error)  => {
+      console.log(error);
+    });
   }
 
 }
